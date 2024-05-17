cb81e29fd85a156146aedb419ccb495d82805b39
Ngoson2004 <86477887+Ngoson2004@users.noreply.github.com>
2024-05-17 17:01:33 +1000

    Add Reservation Controller

--- Controllers/KitchenController.cs
+++ Controllers/KitchenController.cs
@@ -7,3 +7,20 @@ using Assignment_3.Data;
 
 namespace Assignment_3.Controllers;
 
+public class KitchenController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public KitchenController(ApplicationDbContext db)
+    {
+        _db = db;
+
+    }
+    public IActionResult Index()
+    {
+        var orders = _db.Order
+                             .Include(o => o.Items)
+                             .ThenInclude(oi => oi.Menu)
+                             .ToList();
+        return View(orders);
+    }
+}
